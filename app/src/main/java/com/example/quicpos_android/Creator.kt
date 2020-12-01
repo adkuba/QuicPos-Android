c55d92415f0576375c814b26213856c41f7705a3
Jakub Adamski <kuba@exemplum.pl>
2020-12-01 20:38:39 +0100

    navigation-init adkuba/QuicPos-Server#6

--- app/src/main/java/com/example/quicpos_android/Creator.kt
+++ app/src/main/java/com/example/quicpos_android/Creator.kt
@@ -7,5 +7,12 @@ class Creator : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_creator)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
