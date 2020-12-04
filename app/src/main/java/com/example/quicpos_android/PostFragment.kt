3c972436b9bace432f3e1b6ef010b21ebf8f7670
Jakub Adamski <kuba@exemplum.pl>
2020-12-04 19:38:07 +0100

    fragment-post adkuba/QuicPos-Server#6

--- app/src/main/java/com/example/quicpos_android/PostFragment.kt
+++ app/src/main/java/com/example/quicpos_android/PostFragment.kt
@@ -5,8 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.fragment.findNavController
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -18,6 +16,6 @@ class PostFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_post, container, false)
+        return inflater.inflate(R.layout.fragment_postl, container, false)
     }
 }
