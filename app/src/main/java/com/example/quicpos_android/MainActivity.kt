fa8fbbc0152655a58b2073f5ae043fe85632693b
Jakub Adamski <kuba@exemplum.pl>
2020-12-06 17:14:14 +0100

    mode-change-button

--- app/src/main/java/com/example/quicpos_android/MainActivity.kt
+++ app/src/main/java/com/example/quicpos_android/MainActivity.kt
@@ -9,18 +9,41 @@ import android.view.Menu
 import android.view.MenuItem
 import android.widget.Button
 import android.widget.ImageButton
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
+
+    var mode = "NORMAL"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
 
+        //saved
         val savedButton: ImageButton = findViewById(R.id.saved_button)
         savedButton.setOnClickListener{
             val intent = Intent(this, Saved::class.java)
             startActivity(intent)
+        }
+
+        //mode change
+        val privacyButton: ImageButton = findViewById(R.id.privacy_button)
+        privacyButton.setOnClickListener{
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Mode change")
+            if (mode == "NORMAL"){
+                mode = "PRIVATE"
+                builder.setMessage("Going to private mode. Random content, no user data collected.")
+                privacyButton.setImageResource(R.drawable.lock)
+            } else {
+                mode = "NORMAL"
+                builder.setMessage("Going to normal mode. Personalized content, user data collected.")
+                privacyButton.setImageResource(R.drawable.lock_open)
+            }
+            builder.setPositiveButton("Ok", null)
+            val alertDialog: AlertDialog = builder.create()
+            alertDialog.show()
         }
     }
 
