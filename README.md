b2fc465a46382b697d6936c2bce85b5bb0cab65c
Jakub Adamski <kuba@exemplum.pl>
2020-12-08 13:18:10 +0100

    Apollo-init adkuba/QuicPos-Server#6

--- README.md
+++ README.md
@@ -3,3 +3,25 @@
 Z tego co rozumiem aplikacje w Androidzie działają następująco:
   - Najważniejsze są activity - to jest taki jeden ekran, okienko - tak jak na froncie jest to View. Możemy się przemieszczać między różnymi activity. Może być Scroll Activity, Navigation Activity itd.
   - Następnie mamy fragments, są to takie jakby components na frontendzie. Może ich być kilka w ramach jednego activity, a przejścia między nimi może definiować Navigation Controller.
+
+# SSL java
+Przy pobieraniu schama przez apollo graphql wyskoczył mi błąd:
+```
+Execution failed for task ':app:downloadApolloSchema'. > javax.net.ssl.SSLHandshakeException: PKIX path building failed: sun.security.provider.certpath.SunCertPathBuilderException: unable to find valid certification path to requested target
+```
+
+Jest on związany z tym że java nie ma zapisanego certyfikatu <code>www.api.quicpos.com</code>
+
+## Dodawanie certyfikatu do pamięci javy
+1) Java ma zasobnik na klucze, mój był w <code>/etc/ssl/certs/java/cacert</code> Ścieżkę można znaleźć przez wejście do folderu <code></code>
+2) Wchodzimy na moją stronę i pobieramy certyfikat poprzez przeglądarkę (export po wejściu w details certyfikatu) - zapisujemy jako <code>Base64 encoded ASCII single certificate</code>
+3) W folderze w którym jest cacert dodajemy do niego certyfikat komendą:
+<code>
+sudo $JAVA_HOME/bin/keytool -import -alias quicposapi -keystore ./cacerts -file ~/Downloads/www.api.quicpos.com
+</code>
+w Downloads mamy ten pobrany certyfikat.
+4) Robimy reboot, opcjonalnie sprawdzamy za pomocą pliku SSLPoke.class jest u mnie w repo, komendą:
+<code>
+java SSLPoke api.quicpos.com 443
+</code>
+Czy wszystko dobrze się łączy. Powinniśmy dostać success.
--- app/src/main/java/com/example/quicpos_android/MainActivity.kt
+++ app/src/main/java/com/example/quicpos_android/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.quicpos_android
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
@@ -10,10 +12,25 @@ import android.view.MenuItem
 import android.widget.Button
 import android.widget.ImageButton
 import androidx.appcompat.app.AlertDialog
+import androidx.lifecycle.lifecycleScope
+import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.coroutines.await
+import com.apollographql.apollo.coroutines.toDeferred
+import com.apollographql.apollo.exception.ApolloException
+import com.example.GetPostQuery
+import com.example.GetUserQuery
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     var mode = "NORMAL"
+    private val apolloClient: ApolloClient = ApolloClient.builder()
+        .serverUrl("https://api.quicpos.com/query")
+        .build()
+    private val sharedPref: SharedPreferences = getPreferences(Context.MODE_PRIVATE)
+    private val defaultUserID = resources.getInteger(R.integer.default_userid)
+    var userID = sharedPref.getInt(getString(R.string.saved_userid), defaultUserID)
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,6 +61,47 @@ class MainActivity : AppCompatActivity() {
             builder.setPositiveButton("Ok", null)
             val alertDialog: AlertDialog = builder.create()
             alertDialog.show()
+        }
+
+        //getUserID and then posts or only posts if userID exists
+        if (userID == 0){
+            getUser()
+        } else {
+            getPost()
+        }
+    }
+
+
+    fun getPost(){
+        println("USERID: $userID")
+        lifecycleScope.launch {
+            //TODO
+            //save to post1 or post2
+        }
+    }
+
+    private fun getUser(){
+        lifecycleScope.launch {
+            val response = try {
+                apolloClient.query(GetUserQuery("kuba")).await()
+            } catch (e: ApolloException) {
+                println("Apollo error!")
+                return@launch
+            }
+
+            val userid = response.data?.createUser
+            if (userid == null || response.hasErrors()){
+                println("Apollo error2!")
+                return@launch
+            }
+
+            //save to store and var
+            userID = userid
+            with(sharedPref.edit()) {
+                putInt(getString(R.string.saved_userid), userid)
+                apply()
+            }
+            getPost()
         }
     }
 
